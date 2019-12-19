40f3fcf5d28296223fb2d5ca2bf0678cd081981e
Auro Mazzi <auro.mazzi@gmail.com>
2019-12-19 21:03:13 +0100

    Resolve blocking main thread

--- BlokiranjeGlavneNiti/Form.cs
+++ BlokiranjeGlavneNiti/Form.cs
@@ -18,15 +18,19 @@ namespace Vsite.CSharp.DogađajiDelegati
         }
 
         // TODO:070 Na glavnu formu dodati timer (npr. povlačenjem s alatne trake/toolboxa, pod Components) i postaviti ga u aktivno stanje
-
-
         // TODO:071 U glavnu formu dodati metodu koja će biti okinuta togađajem Tick objekta timer. Unutar metode treba ažurirati sadržaj kontrole textBoxTime tako da prikazuje vrijeme u sekundama, pozivom metode DateTime.Now.ToLongTimeString();
-
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBoxTime.Text = DateTime.Now.ToLongTimeString();
+        }
 
         // TODO:072 U glavnu formu dodati metodu koja će biti okinuta događajem Click tipke buttonBlock. Metoda treba zaustaviti izvođenje glavne niti na 10 sekundi pozivom metode Thread.Sleep(10000)
-
-
         // TODO:073 Pokrenuti program i provjeriti odziv forme nakon pritiska tipke Block
+        private void buttonBlock_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(10000);
+        }
 
+        
     }
 }
