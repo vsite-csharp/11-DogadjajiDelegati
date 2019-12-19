95b801df04f6901ef7c39584cc9242cbece1bc8c
Domagoj Beronic <domagojberonic@outlook.com>
2019-12-19 20:03:55 +0100

    Resolve delegte chaining

--- ProsljedjivanjeDelegata/ProsljeđivanjeDelegata.cs
+++ ProsljedjivanjeDelegata/ProsljeđivanjeDelegata.cs
@@ -27,14 +27,13 @@ namespace Vsite.CSharp.DogađajiDelegati
         {
             
             IspišiFunkciju(Math.Sin, 0, Math.PI, 10);
-            // TODO:021 Pokrenuti program i provjeriti ispis
+          
         }
 
         public static void IspisFunkcijeE()
         {
-            // TODO:022 Pozvati metodu IspišiFunkciju tako da ispiše vrijednosti funkcije Math.Exp za 5 točaka u intervalu od 0 do 10
+            
             IspišiFunkciju(Math.Exp, 0, 10, 5);
-            // TODO:023 Pokrenuti program i provjeriti ispis
         }
 
         // TODO:024 Pokrenuti i provjeriti testove (2 testa u grupi "TestProsljeđivanjaDelegata" moraju proći).
