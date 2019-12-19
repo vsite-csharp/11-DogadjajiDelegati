d62c98e04dadfa8e19e28419df4a5b5daf72c4ba
Robert Bakota <robakota@vsite.hr>
2019-12-19 19:51:19 +0100

    Resolve delegates

--- PretplataNaDogadjaje/EventsForm.cs
+++ PretplataNaDogadjaje/EventsForm.cs
@@ -39,9 +39,4 @@ namespace Vsite.CSharp.DogađajiDelegati
             this.Close();
         }
     }
-
-
-
-    // TODO:006 Pokrenuti i provjeriti testove (4 testa u grupi "TestPretplateNaDogađaje" moraju proći)
-
 }
--- PridruzivanjeMetodaIPozivDelegata/PridruživanjeMetodaIPozivDelegata.cs
+++ PridruzivanjeMetodaIPozivDelegata/PridruživanjeMetodaIPozivDelegata.cs
@@ -28,27 +28,26 @@ namespace Vsite.CSharp.DogađajiDelegati
 
         public static void PozivStatičkeMetodePrekoDelegata()
         {
-            // TODO:010 Instancirati delegata tipa MojDelegat metodom StatičkaMetoda te pozvati delegata.
-
-            // TODO:011 Pokrenuti program i pogledati ispis.
+            MojDelegat md = StatičkaMetoda;
+            md();
         }
 
         public static void PozivMetodeInstancePrekoDelegata()
         {
-            // TODO:012 Instancirati delegata tipa MojDelegat metodom MetodaInstance te pozvati delegata. 
-
-            // TODO:013 Pokrenuti program i provjeriti ispis.
+            PridruživanjeMetodaIPozivDelegata pm = new PridruživanjeMetodaIPozivDelegata();
+            MojDelegat md = pm.MetodaInstance;
+            md();
         }
 
         public static void PozivStatičkeIDvijeMetodeInstancePrekoDelegata()
         {
-            // TODO:014 Instancirati delegata tipa MojDelegat metodom MetodaInstance. 
+            PridruživanjeMetodaIPozivDelegata pm = new PridruživanjeMetodaIPozivDelegata();
+            MojDelegat md = pm.MetodaInstance;
 
-            // TODO:015 Delegatu dodati metodu Statička metoda i metodu DrugaMetodaInstance te pozvati delegata.
+            md += StatičkaMetoda;
+            md += pm.DrugaMetodaInstance;
+            md();
 
-            // TODO:016 Pokrenuti program i provjeriti ispis.
-
-            // TODO:017 Pokrenuti i provjeriti testove (3 testa u grupi "TestDelegatskihMetoda" moraju proći).
         }
 
         static void Main(string[] args)
