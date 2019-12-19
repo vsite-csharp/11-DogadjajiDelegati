a36e109524f91dadf998e0919fd557787fdd0f32
Robert Bakota <robakota@vsite.hr>
2019-12-19 20:20:38 +0100

    Resolve Invocation list

--- UlancavanjeDelegata/UlančavanjeDelegata.cs
+++ UlancavanjeDelegata/UlančavanjeDelegata.cs
@@ -46,8 +46,6 @@ namespace Vsite.CSharp.DogađajiDelegati
 
         public static TextWriter writer;
 
-        // TODO:034 Pokrenite i provjerite testove (dva testa u grupi "TestUlančavanjaDelegata" moraju proći)
-
         static void Main(string[] args)
         {
             IspisPoruka("Dobar dan", "Bok", "Guten Tag");
