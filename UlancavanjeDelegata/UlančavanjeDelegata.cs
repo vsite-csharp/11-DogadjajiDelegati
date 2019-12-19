6ff349c25f7d55ae6ada6e58d0e59e8b7f0b1a34
JosipVuckovic <josip.vuckovic@live.com>
2019-12-19 20:04:01 +0100

    Solved UlančavanjeDelegata and all tasks passed

--- UlancavanjeDelegata/UlančavanjeDelegata.cs
+++ UlancavanjeDelegata/UlančavanjeDelegata.cs
@@ -25,20 +25,12 @@ namespace Vsite.CSharp.DogađajiDelegati
         public static void IspisPoruka(params string[] poruke)
         {
             IspišiPoruku ispisPoruke = IspišiUFajl;
-
+           
             ispisPoruke(poruke[0]);
 
-            // TODO:030 Pokrenuti program i pogledati sadržaj datoteke "poruke.txt" (u potkazalu bin\Debug).
-
-            // TODO:031 Dodajte delegatu metodu IspišiNaKonzolu.
-
-
+            ispisPoruke += IspišiNaKonzolu;
             ispisPoruke(poruke[1]);
-
-            // TODO:032 Pokrenite program i usporedite prikaz na konzoli sa sadržajem datoteke "poruke.txt".
-
-            // TODO:033 Iz delegata uklonite IspišiUFajl, pokrenite program i provjerite sadržaj datoteke "poruke.txt".
-
+            ispisPoruke -= IspišiUFajl;
 
             ispisPoruke(poruke[2]);
         }
