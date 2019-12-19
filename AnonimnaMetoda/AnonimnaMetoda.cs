f87148e110f6b6deb1530741d7e181fb33926ce0
JosipVuckovic <josip.vuckovic@live.com>
2019-12-19 21:01:20 +0100

    Solved BlokiranjeGlavneNiti and AnonimnaMetoda, no tests

--- AnonimnaMetoda/AnonimnaMetoda.cs
+++ AnonimnaMetoda/AnonimnaMetoda.cs
@@ -24,15 +24,25 @@ namespace Vsite.CSharp.DogađajiDelegati
 
         static void Main(string[] args)
         {
-            // TODO:060 Pokrenuti program i pogledati ispis.
-            Thread nit = new Thread(NitnaMetoda);
+            //Thread nit = new Thread(NitnaMetoda);
 
-            // TODO:061 Umjesto gornje naredbe u kojoj se prosljeđuje NitnaMetoda, novoj niti proslijediti anonimnu metodu definiranu pomoću delegata. Pokrenuti program i pogledati ispis.
-            // Thread nit = new Thread(delegate() ... );
+            //Thread nit = new Thread(delegate ()
+            //{
+            //    Console.WriteLine("Krenula je nova nit...");
+            //    for (int i = 0; i < max; ++i)
+            //    {
+            //        Console.Write(format, i);
+            //    }
+            //});
 
-            // TODO:062 Anonimnu metodu zadanu delegatom nadomjestiti jednostavnim lambda izrazom. Pokrenuti program i pogledati ispis.
-
-            nit.Start();
+            Thread nit = new Thread(()=> {
+                    Console.WriteLine("Krenula je nova nit...");
+                  for (int i = 0; i < max; ++i)
+                  {
+                    Console.Write(format, i);
+                  }
+               });
+                nit.Start();
 
             for (int i = 0; i < max; ++i)
             {
