cab56140f2b09b5610391cde4ff3e865dbf12e45
db <darij.b@gmail.com>
2019-12-19 20:21:23 +0100

    Resolved Invocation  List

--- GetInvocationList/GetInvocationList.cs
+++ GetInvocationList/GetInvocationList.cs
@@ -11,22 +11,28 @@ namespace Vsite.CSharp.DogađajiDelegati
     {
         public delegate double Funkcija(double x);
 
-        static void IspišiFunkcijeUPaketu(Funkcija fja, double x1, double x2, int n)
+        static void IspišiFunkcijeUPaketu(Funkcija fje, double x1, double x2, int n)
         {
-            Console.WriteLine("Ispis funkcije {0}:", fja.Method);
-            Console.WriteLine("{0,4} {1,20}", "x", "y");
-            for (int i = 0; i < n; ++i)
-            {
-                double x = x1 + (x2 - x1) / (n - 1) * i;
-                Console.WriteLine("{0,15:0.0000000000} {1,20:0.0000000000}", x, fja(x));
+            
+            { 
+                Console.WriteLine("Ispis funkcije {0}:", fje.Method);
+                Console.WriteLine("{0,4} {1,20}", "x", "y");
+                for (int i = 0; i < n; ++i)
+                {
+                    double x = x1 + (x2 - x1) / (n - 1) * i;
+                    Console.WriteLine("{0,15:0.0000000000} {1,20:0.0000000000}", x, fje.DynamicInvoke(x));
+                }
+
             }
+           
         }
 
         public static void IspišiFunkcijeZasebno(Funkcija fja, double x1, double x2, int n)
         {
             // TODO:041 Pomoću metode GetInvocationList dohvatiti sve delegatske funkcije zasebno i za svaku ispisati vrijednosti u traženom intervalu
-
-
+            var listaFja = fja.GetInvocationList();
+            foreach (var f in listaFja)
+                IspišiFunkcijeUPaketu((Funkcija)f,x1,x2,n);
             // TODO:042 Pokrenuti program i provjeriti ispis.
         }
 
