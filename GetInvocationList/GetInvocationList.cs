8fb5fef548737fa77c94aac0e4d3427bfbacdaf7
Marko Bohač <mabohac@vsite.hr>
2019-12-20 13:31:31 +0100

    Resolved GetInvocationList

--- GetInvocationList/GetInvocationList.cs
+++ GetInvocationList/GetInvocationList.cs
@@ -11,41 +11,41 @@ namespace Vsite.CSharp.DogađajiDelegati
     {
         public delegate double Funkcija(double x);
 
-        static void IspišiFunkcijeUPaketu(Funkcija fja, double x1, double x2, int n)
+        static void IspišiFunkcijeUPaketu(Funkcija fje, double x1, double x2, int n)
         {
-            Console.WriteLine("Ispis funkcije {0}:", fja.Method);
+            Console.WriteLine("Ispis funkcije {0}:", fje.Method);
             Console.WriteLine("{0,4} {1,20}", "x", "y");
             for (int i = 0; i < n; ++i)
             {
                 double x = x1 + (x2 - x1) / (n - 1) * i;
-                Console.WriteLine("{0,15:0.0000000000} {1,20:0.0000000000}", x, fja(x));
+                Console.WriteLine("{0,15:0.0000000000} {1,20:0.0000000000}", x, fje.DynamicInvoke(x));
             }
         }
 
-        public static void IspišiFunkcijeZasebno(Funkcija fja, double x1, double x2, int n)
+        public static void IspišiFunkcijeZasebno(Funkcija fje, double x1, double x2, int n)
         {
-            // TODO:041 Pomoću metode GetInvocationList dohvatiti sve delegatske funkcije zasebno i za svaku ispisati vrijednosti u traženom intervalu
-
-
-            // TODO:042 Pokrenuti program i provjeriti ispis.
+            // Pomoću metode GetInvocationList dohvatiti sve delegatske funkcije zasebno i za svaku ispisati vrijednosti u traženom intervalu
+            var listaFja = fje.GetInvocationList();
+            foreach (var f in listaFja)
+                IspišiFunkcijeUPaketu((Funkcija)f, x1, x2, n);
+            // Pokrenuti program i provjeriti ispis.
         }
+            // Pokrenuti i provjeriti testove (test u grupi "TestGetInvocationList" mora proći)
 
-        // TODO:043 Pokrenuti i provjeriti testove (test u grupi "TestGetInvocationList" mora proći)
+            static void Main(string[] args)
+            {
+                // Pokrenuti program i provjeriti ispis
+                Funkcija f = Math.Cos;
+                f += Math.Sin;
 
-        static void Main(string[] args)
-        {
-            // TODO:040 Pokrenuti program i provjeriti ispis
-            Funkcija f = Math.Cos;
-            f += Math.Sin;
+                Console.WriteLine("*** IspišiFunkcijeUPaketu ***");
+                IspišiFunkcijeUPaketu(f, 0, Math.PI, 10);
 
-            Console.WriteLine("*** IspišiFunkcijeUPaketu ***");
-            IspišiFunkcijeUPaketu(f, 0, Math.PI, 10);
+                Console.WriteLine("*** IspišiFunkcijeZasebno ***");
+                IspišiFunkcijeZasebno(f, 0, Math.PI, 10);
 
-            Console.WriteLine("*** IspišiFunkcijeZasebno ***");
-            IspišiFunkcijeZasebno(f, 0, Math.PI, 10);
-
-            Console.WriteLine("GOTOVO!!!");
-            Console.ReadKey();
-        }
+                Console.WriteLine("GOTOVO!!!");
+                Console.ReadKey();
+            }
     }
 }
