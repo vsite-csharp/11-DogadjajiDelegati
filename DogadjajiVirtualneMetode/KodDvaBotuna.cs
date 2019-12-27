944057e25c3d8c4e583a843881fa5e5139938527
Domagoj Salkić <48859348+domagojsalkic@users.noreply.github.com>
2019-12-27 16:52:12 +0100

    Resolve event virtual methods

--- DogadjajiVirtualneMetode/KodDvaBotuna.cs
+++ DogadjajiVirtualneMetode/KodDvaBotuna.cs
@@ -15,8 +15,8 @@ namespace Vsite.CSharp.DogađajiDelegati
         public KodDvaBotuna()
         {
             InitializeComponent();
-            // TODO:100 Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy
-
+            button.Click += Button_Click;
+            buttonMy.Click += ButtonMy_Click;
         }
 
         private void Button_Click(object sender, EventArgs e)
@@ -29,10 +29,13 @@ namespace Vsite.CSharp.DogađajiDelegati
             MessageBox.Show("Kliknuo si me!", "Moj botun");
         }
 
-        // TODO:101 Napraviti klasu MyButton koja će biti izvedena iz klase Button, u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox
-
-        // TODO:102 Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke
-
-        // TODO:103 U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
+        class MyButton : Button
+        {
+            protected override void OnClick(EventArgs e)
+            {
+                MessageBox.Show("Kliknuo si myButton", "Moj button");
+                //base.OnClick(e);
+            }
+        }
     }
 }
