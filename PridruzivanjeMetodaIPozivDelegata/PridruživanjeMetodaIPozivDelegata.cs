50651d29b02c78b16524407c961e946978d04663
IvanBukvic <32536551+IvanBukvic@users.noreply.github.com>
2021-01-07 19:36:16 +0100

    Komit

--- PridruzivanjeMetodaIPozivDelegata/PridruživanjeMetodaIPozivDelegata.cs
+++ PridruzivanjeMetodaIPozivDelegata/PridruživanjeMetodaIPozivDelegata.cs
@@ -28,27 +28,33 @@ namespace Vsite.CSharp.DogađajiDelegati
 
         public static void PozivStatičkeMetodePrekoDelegata()
         {
-            // TODO:010 Instancirati delegata tipa MojDelegat metodom StatičkaMetoda te pozvati delegata.
-
-            // TODO:011 Pokrenuti program i pogledati ispis.
+            //  Instancirati delegata tipa MojDelegat metodom StatičkaMetoda te pozvati delegata.
+            MojDelegat mojDelegat = new MojDelegat(StatičkaMetoda);
+            mojDelegat();
+            //  Pokrenuti program i pogledati ispis.
         }
 
         public static void PozivMetodeInstancePrekoDelegata()
         {
-            // TODO:012 Instancirati delegata tipa MojDelegat metodom MetodaInstance te pozvati delegata. 
-
-            // TODO:013 Pokrenuti program i provjeriti ispis.
+            //  Instancirati delegata tipa MojDelegat metodom MetodaInstance te pozvati delegata. 
+            PridruživanjeMetodaIPozivDelegata obj = new PridruživanjeMetodaIPozivDelegata();
+            MojDelegat mojDelegat = new MojDelegat(obj.MetodaInstance);
+            mojDelegat();
+            //  Pokrenuti program i provjeriti ispis.
         }
 
         public static void PozivStatičkeIDvijeMetodeInstancePrekoDelegata()
         {
-            // TODO:014 Instancirati delegata tipa MojDelegat metodom MetodaInstance. 
+            //  Instancirati delegata tipa MojDelegat metodom MetodaInstance. 
+            PridruživanjeMetodaIPozivDelegata obj = new PridruživanjeMetodaIPozivDelegata();
+            MojDelegat mojDelegat = new MojDelegat(obj.MetodaInstance);
+            //  Delegatu dodati metodu Statička metoda i metodu DrugaMetodaInstance te pozvati delegata.
+            mojDelegat += obj.DrugaMetodaInstance;
+            mojDelegat += StatičkaMetoda;
+            mojDelegat();
+            //  Pokrenuti program i provjeriti ispis.
 
-            // TODO:015 Delegatu dodati metodu Statička metoda i metodu DrugaMetodaInstance te pozvati delegata.
-
-            // TODO:016 Pokrenuti program i provjeriti ispis.
-
-            // TODO:017 Pokrenuti i provjeriti testove (3 testa u grupi "DelegatskeMetode" moraju proći).
+            //  Pokrenuti i provjeriti testove (3 testa u grupi "DelegatskeMetode" moraju proći).
         }
 
         static void Main(string[] args)
