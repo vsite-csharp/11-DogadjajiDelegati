9204607dff630da42af90408ffefd51205c34094
Mislav <mislav.miseric@gmail.com>
2021-01-07 20:51:38 +0100

    Resolve DefiniranjeDogađaja

--- DefiniranjeDogadjaja/AlarmEventArgs.cs
+++ DefiniranjeDogadjaja/AlarmEventArgs.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
-    //          U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
 
-    public class AlarmEventArgs
+    public class AlarmEventArgs : EventArgs
     {
+        public readonly DateTime Vrijeme;
+        public readonly string Mjesto;
+        public readonly int Razina;
+        public readonly string Opis;
+
+        public AlarmEventArgs(string mjesto, int razina, string opis)
+        {
+            Vrijeme = DateTime.Now;
+            Mjesto = mjesto;
+            Razina = razina;
+            Opis = opis;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var args = obj as AlarmEventArgs;
+            return args != null &&
+                   Vrijeme == args.Vrijeme &&
+                   Mjesto == args.Mjesto &&
+                   Razina == args.Razina &&
+                   Opis == args.Opis;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -608843544;
+            hashCode = hashCode * -1521134295 + Vrijeme.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Mjesto);
+            hashCode = hashCode * -1521134295 + Razina.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Opis);
+            return hashCode;
+        }
     }
 }
--- DefiniranjeDogadjaja/GeneratorAlarma.cs
+++ DefiniranjeDogadjaja/GeneratorAlarma.cs
@@ -5,17 +5,20 @@ using System.Text;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:060 U klasi GeneratorAlarma definirati događaj Alarm s delegatom tipa EventHandler te metodu protected virtual void OnAlarm(EventArgs e)
-    //           koja treba okidati događaj Alarm
-
-    // TODO:065 U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
-    // TODO:066 Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
     class GeneratorAlarma
     {
-        // TODO:061 U metodu DižiAlarm dodati poziv metode OnAlarm.
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+        public event AlarmEventHandler Alarm;
+
+        protected virtual void OnAlarm(AlarmEventArgs e)
+        {
+            Alarm?.Invoke(this, e);
+        }
+
         public void DižiAlarm(string mjesto, int razina, string opis)
         {
             Console.WriteLine($"!!!Generator diže alarm {DateTime.Now}: mjesto={mjesto}, razina={razina}, opis={opis}!!!");
+            OnAlarm(new AlarmEventArgs(mjesto, razina, opis));
         }
     }
 }
