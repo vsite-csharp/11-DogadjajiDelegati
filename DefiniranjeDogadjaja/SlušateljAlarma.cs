47407686e51315aa6b9dbb26c7a911c2a18dc941
Valentin Vareskic <valentin.vareskic@outlook.com>
2021-01-07 21:04:22 +0100

    Resolve memory leak when object doesn't unsubscribes

--- DefiniranjeDogadjaja/SlušateljAlarma.cs
+++ DefiniranjeDogadjaja/SlušateljAlarma.cs
@@ -12,8 +12,6 @@ namespace Vsite.CSharp.DogađajiDelegati
             Console.WriteLine("***Dobivena je notifikacija o alarmu***");
             Console.WriteLine($"***{e.Vrijeme}: mjesto={e.Mjesto}, razina={e.Razina}, opis={e.Opis}");
         }
-        // TODO:067 Modificirati metodu AlarmNotifikacija tako da ispiše sve podatke o alarmu (vrijeme, mjesto, razinu i opis). Pokrenuti program i provjeriti ispis.
     }
 
-    // TODO:068 Pokrenuti testove u grupi DefiniranjeDogađaja (5 testova mora proći)
 }
