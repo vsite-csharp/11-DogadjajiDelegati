5cd107a1373618c3aaf16cd9d0d842c0bd6656e4
IvanBukvic <32536551+IvanBukvic@users.noreply.github.com>
2021-01-07 20:56:10 +0100

    Komit

--- DefiniranjeDogadjaja/AlarmEventArgs.cs
+++ DefiniranjeDogadjaja/AlarmEventArgs.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
+    //  Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
     //          U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
 
-    public class AlarmEventArgs
+    public class AlarmEventArgs : EventArgs
     {
+        public readonly DateTime vrijeme;
+        public readonly string mjesto;
+        public readonly int razina;
+        public readonly string opis;
+
+        
+        public AlarmEventArgs(string mjesto, int razina, string opis)
+        {
+            this.vrijeme = DateTime.Now;
+            this.mjesto = mjesto;
+            this.razina = razina;
+            this.opis = opis;
+        }
     }
 }
