95253bd3d2bdd94b72ae9a5fad60438de0775278
MajaSm <maja.smolkovic@gmail.com>
2021-01-07 21:04:01 +0100

    Resolve memory leak

--- CurenjeMemorijeZbogNeodjave/CurenjeMemorijeZbogNeodjave.cs
+++ CurenjeMemorijeZbogNeodjave/CurenjeMemorijeZbogNeodjave.cs
@@ -46,12 +46,12 @@ namespace CurenjeMemorijeZbogNeodjave
             {
                 SlušateljDogađaja sd = new SlušateljDogađaja();
 
-                // TODO:070 Pokrenuti kod i provjeriti ispis na konzolu.
+                //  Pokrenuti kod i provjeriti ispis na konzolu.
 
-                // TODO:071 Dodati naredbu kojom se objekt sd predbilježi na slušanje događaja, pokrenuti kod i provjeriti ispis.
-
-                // TODO:072 Dodati naredbu kojom se objekt sd odjavljuje od slušanja događaja, pokrenuti kod i provjeriti ispis.
-
+                //  Dodati naredbu kojom se objekt sd predbilježi na slušanje događaja, pokrenuti kod i provjeriti ispis.
+                gd.Događaj += sd.DogađajEventHandler;
+                //  Dodati naredbu kojom se objekt sd odjavljuje od slušanja događaja, pokrenuti kod i provjeriti ispis.
+                gd.Događaj -= sd.DogađajEventHandler;
             }
 
             Console.WriteLine($"Zauzeta memorija na heapu: {GC.GetTotalMemory(true) / 1024.0} kB");
