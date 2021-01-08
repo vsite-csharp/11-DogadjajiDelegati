d8808b76519fecf6c4f1fa226dbc1dfe3dca6490
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-01-08 10:25:40 +0100

    Resolve event definition

--- DefiniranjeDogadjaja/GeneratorAlarma.cs
+++ DefiniranjeDogadjaja/GeneratorAlarma.cs
@@ -5,17 +5,32 @@ using System.Text;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:060 U klasi GeneratorAlarma definirati događaj Alarm s delegatom tipa EventHandler te metodu protected virtual void OnAlarm(EventArgs e)
+    // U klasi GeneratorAlarma definirati događaj Alarm s delegatom tipa EventHandler te metodu protected virtual void OnAlarm(EventArgs e)
     //           koja treba okidati događaj Alarm
 
-    // TODO:065 U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
-    // TODO:066 Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
+    // U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
+    // Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
     class GeneratorAlarma
     {
-        // TODO:061 U metodu DižiAlarm dodati poziv metode OnAlarm.
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+
+        // public event EventHandler Alarm; vise nije event nego AlarmEventHandler dolje
+        public event AlarmEventHandler Alarm;
+
+        //protected virtual void OnAlarm(EventArgs e)
+        protected virtual void OnAlarm(AlarmEventArgs e)
+        {
+            //if (Alarm != null)
+            //    Alarm(this, e);
+
+            Alarm?.Invoke(this, e);
+        }
+        // U metodu DižiAlarm dodati poziv metode OnAlarm.
         public void DižiAlarm(string mjesto, int razina, string opis)
         {
             Console.WriteLine($"!!!Generator diže alarm {DateTime.Now}: mjesto={mjesto}, razina={razina}, opis={opis}!!!");
+            //OnAlarm(EventArgs.Empty);
+            OnAlarm(new AlarmEventArgs(mjesto, razina, opis));
         }
     }
 }
