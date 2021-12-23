10a20c8180dbdad2d0032a7abcbd5d4c67e39282
Pavle Pavicic <ppavicic@dspace.hr>
2021-12-23 21:14:41 +0100

    Resolved event definition.

--- DefiniranjeDogadjaja/SlušateljAlarma.cs
+++ DefiniranjeDogadjaja/SlušateljAlarma.cs
@@ -5,13 +5,20 @@ using System.Text;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:062 U klasu SlušateljAlarma dodati metodu AlarmNotifikacija koja će se moći predbilježiti na događaj Alarm u klasi GeneratorAlarma. 
+    // U klasu SlušateljAlarma dodati metodu AlarmNotifikacija koja će se moći predbilježiti na događaj Alarm u klasi GeneratorAlarma. 
     // Unutar te metode na konzolu ispisati poruku da je dobivena poruka o alarmu
     class SlušateljAlarma
     {
+        public void AlarmNotifikacija(object sender, AlarmEventArgs e)
+        {
+            Console.WriteLine(e.Mjesto);
+            Console.WriteLine(e.Vrijeme);
+            Console.WriteLine(e.Razina);
+            Console.WriteLine(e.Opis);
+        }
 
-        // TODO:068 Modificirati metodu AlarmNotifikacija tako da ispiše sve podatke o alarmu (vrijeme, mjesto, razinu i opis). Pokrenuti program i provjeriti ispis.
+        // Modificirati metodu AlarmNotifikacija tako da ispiše sve podatke o alarmu (vrijeme, mjesto, razinu i opis). Pokrenuti program i provjeriti ispis.
     }
 
-    // TODO:069 Pokrenuti testove u grupi DefiniranjeDogađaja (5 testova mora proći).
+    // Pokrenuti testove u grupi DefiniranjeDogađaja (5 testova mora proći).
 }
