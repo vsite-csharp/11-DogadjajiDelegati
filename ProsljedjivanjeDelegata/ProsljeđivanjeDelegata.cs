5c0c4e0174c704c156b430cd2c45e5e050089708
Matteo Madirazza <mamadira@vsite.hr>
2021-12-23 20:15:01 +0100

    resolve delegates chaining

--- ProsljedjivanjeDelegata/ProsljeđivanjeDelegata.cs
+++ ProsljedjivanjeDelegata/ProsljeđivanjeDelegata.cs
@@ -33,7 +33,7 @@ namespace Vsite.CSharp.DogađajiDelegati
         public static void IspisFunkcijeE()
         {
             // :022 Pozvati metodu IspišiFunkciju tako da ispiše vrijednosti funkcije Math.Exp za 5 točaka u intervalu od 0 do 10.
-            IspišiFunkciju(Math.Exp,0,10,10);
+            IspišiFunkciju(Math.Exp,0,10,5);
             // :023 Pokrenuti program i provjeriti ispis
         }
 
