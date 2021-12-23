787a11d9d3dcecfce1e22b9f6777eb1305577807
Josip Romac <josipromac@gmail.com>
2021-12-23 21:15:23 +0100

    Resolved Event definition

--- DefiniranjeDogadjaja/GeneratorAlarma.cs
+++ DefiniranjeDogadjaja/GeneratorAlarma.cs
@@ -5,16 +5,30 @@ using System.Text;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:060 U klasi GeneratorAlarma definirati događaj Alarm s delegatom tipa EventHandler te metodu protected virtual void OnAlarm(EventArgs e) koja treba okidati događaj Alarm.
+    // :060 U klasi GeneratorAlarma definirati događaj Alarm s delegatom tipa EventHandler te metodu protected virtual void OnAlarm(EventArgs e) koja treba okidati događaj Alarm.
 
-    // TODO:066 U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
-    // TODO:067 Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
+    // :066 U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
+    // :067 Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
     class GeneratorAlarma
     {
-        // TODO:061 U metodu DižiAlarm dodati poziv metode OnAlarm.
+        //public event EventHandler Alarm;
+        public event AlarmEventHandler Alarm;
+
+
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs a);
+
+        protected virtual void OnAlarm(AlarmEventArgs e)
+        {
+            //Alarm?.Invoke(this,e) isto kao i uvjet ispod
+            if (Alarm != null)
+                Alarm(this, e);
+        }
+        // :061 U metodu DižiAlarm dodati poziv metode OnAlarm.
         public void DižiAlarm(string mjesto, int razina, string opis)
         {
             Console.WriteLine($"!!!Generator diže alarm {DateTime.Now}: mjesto={mjesto}, razina={razina}, opis={opis}!!!");
+            // OnAlarm(EventArgs.Empty);
+            OnAlarm(new AlarmEventArgs(mjesto, razina, opis));
         }
     }
 }
