f53e48f10dc83c5b458df926e16c21df1c977711
Ivan Sokolović <isokolovic1@gmail.com>
2021-12-23 20:22:11 +0100

    Resolve GetInvocationList

--- GetInvocationList/GetInvocationList.cs
+++ GetInvocationList/GetInvocationList.cs
@@ -24,16 +24,20 @@ namespace Vsite.CSharp.DogađajiDelegati
 
         public static void IspišiFunkcijeZasebno(Funkcija fja, double x1, double x2, int n)
         {
-            // TODO:041 Pomoću metode GetInvocationList dohvatiti sve delegatske funkcije zasebno i za svaku ispisati vrijednosti u traženom intervalu
+            // :041 Pomoću metode GetInvocationList dohvatiti sve delegatske funkcije zasebno i za svaku ispisati vrijednosti u traženom intervalu
+            foreach (Funkcija f in fja.GetInvocationList())
+            {
+                IspišiFunkciju(f, x1, x2, n);
+            }
 
-            // TODO:042 Pokrenuti program i provjeriti ispis.
+            // :042 Pokrenuti program i provjeriti ispis.
         }
 
-        // TODO:043 Pokrenuti i provjeriti testove (test u grupi "GetInvocationList" mora proći)
+        // :043 Pokrenuti i provjeriti testove (test u grupi "GetInvocationList" mora proći)
 
         static void Main(string[] args)
         {
-            // TODO:040 Pokrenuti program i provjeriti ispis
+            // :040 Pokrenuti program i provjeriti ispis
             Funkcija f = Math.Cos;
             f += Math.Sin;
 
