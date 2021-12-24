ff4883a09a0974cb0a63b1630aaf55ac8ab8baf3
Antonio Brkic <ABrkic@dspace.hr>
2021-12-24 13:49:25 +0100

    Resolve DZ virtual methods events

--- DogadjajiVirtualneMetode/KodDvaBotuna.cs
+++ DogadjajiVirtualneMetode/KodDvaBotuna.cs
@@ -15,10 +15,9 @@ namespace Vsite.CSharp.DogađajiDelegati
         public KodDvaBotuna()
         {
             InitializeComponent();
-            // TODO:080 Pokrenuti program i pogledati formu i kontrole na njoj.
+            // 080 Pokrenuti program i pogledati formu i kontrole na njoj.
 
-            // TODO:081 Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy.
-
+            // 081 Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy.
         }
 
         private void Button_Click(object sender, EventArgs e)
@@ -31,10 +30,10 @@ namespace Vsite.CSharp.DogađajiDelegati
             MessageBox.Show("Kliknuo si me!", "Moj botun");
         }
 
-        // TODO:082 Napraviti klasu MyButton koja će biti izvedena iz klase Button, u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox.
+        // 082 Napraviti klasu MyButton koja će biti izvedena iz klase Button, u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox.
 
-        // TODO:083 Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke.
+        // 083 Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke.
 
-        // TODO:084 U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
+        // 084 U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
     }
 }
---
+++ DogadjajiVirtualneMetode/MojBotun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vsite.CSharp.DogađajiDelegati
+{
+    class MojBotun : Button
+    {
+        protected override void OnClick(EventArgs e)
+        {
+            //base.OnClick(e);
+            MessageBox.Show("Kliknuo si me!", "Moj botun");
+        }
+    }
+}
