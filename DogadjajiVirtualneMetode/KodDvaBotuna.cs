b092c2a1e0da609138f5dbc9bb291214f7afd5d6
Pavle Pavicic <ppavicic@dspace.hr>
2021-12-24 15:34:34 +0100

    Solved all TODO:08*.

--- DogadjajiVirtualneMetode/KodDvaBotuna.cs
+++ DogadjajiVirtualneMetode/KodDvaBotuna.cs
@@ -15,10 +15,11 @@ namespace Vsite.CSharp.DogađajiDelegati
         public KodDvaBotuna()
         {
             InitializeComponent();
-            // TODO:080 Pokrenuti program i pogledati formu i kontrole na njoj.
+            // Pokrenuti program i pogledati formu i kontrole na njoj.
 
-            // TODO:081 Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy.
-
+            // Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy.
+            this.button.Click += Button_Click;
+            this.buttonMy.Click += ButtonMy_Click;
         }
 
         private void Button_Click(object sender, EventArgs e)
@@ -31,10 +32,19 @@ namespace Vsite.CSharp.DogađajiDelegati
             MessageBox.Show("Kliknuo si me!", "Moj botun");
         }
 
-        // TODO:082 Napraviti klasu MyButton koja će biti izvedena iz klase Button, u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox.
+        // Napraviti klasu MyButton koja će biti izvedena iz klase Button, u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox.
+        public class MyButton : Button
+        {
+            protected override void OnClick(EventArgs e)
+            {
+                //base.OnClick(e);
+                MessageBox.Show("Kliknuo si me!", "Nadglasujući botun");
+            }
+        }
+        // Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke.
 
-        // TODO:083 Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke.
-
-        // TODO:084 U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
+        // U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
     }
+
+
 }
