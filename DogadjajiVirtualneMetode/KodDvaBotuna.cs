e3ce14db9c0b78ff45fe7272f90b45d773b3a910
Marina Letica <MLetica@dspace.hr>
2021-12-27 17:24:14 +0100

    Resolved events

--- DogadjajiVirtualneMetode/KodDvaBotuna.cs
+++ DogadjajiVirtualneMetode/KodDvaBotuna.cs
@@ -15,9 +15,11 @@ namespace Vsite.CSharp.DogađajiDelegati
         public KodDvaBotuna()
         {
             InitializeComponent();
-            // TODO:080 Pokrenuti program i pogledati formu i kontrole na njoj.
+            // :080 Pokrenuti program i pogledati formu i kontrole na njoj.
+            button.Click += Button_Click;
+            buttonMy.Click += ButtonMy_Click;
 
-            // TODO:081 Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy.
+            // :081 Pridružiti donja dva rukovatelja događajima Click tipki button i buttonMy.
 
         }
 
@@ -31,10 +33,20 @@ namespace Vsite.CSharp.DogađajiDelegati
             MessageBox.Show("Kliknuo si me!", "Moj botun");
         }
 
-        // TODO:082 Napraviti klasu MyButton koja će biti izvedena iz klase Button, u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox.
+        // :082 Napraviti klasu MyButton koja će biti izvedena iz klase Button,
+        // u njoj nadglasati virtualnu metodu OnClick te unutar metode dodati prikaz kontrole MessageBox.
 
-        // TODO:083 Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke.
+        // :083 Promijeniti objekt buttonMy u formi tako da bude tipa MyButton, pokrenuti kod i provjeriti ponašanje tipke.
 
-        // TODO:084 U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
+        // :084 U metodi OnClick klase MyButton zakomentirati poziv base.OnClick te provjeriti ponašanje tipke nakon toga.
+    }
+
+    public class MyButton : Button
+    {
+        protected override void OnClick(EventArgs e)
+        {
+            //base.OnClick(e);
+            MessageBox.Show("Kliknuo si me!", "Moj botun iz vanjske");
+        }
     }
 }
