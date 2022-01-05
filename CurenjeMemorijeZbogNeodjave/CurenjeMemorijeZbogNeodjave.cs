79860585251c57202bf72b8ba8a192d6cb315ecc
Ana Lovric <alovric@dspace.hr>
2022-01-05 15:14:28 +0100

    DZ: Resolved Memory leaking due to unsubscribing.

--- CurenjeMemorijeZbogNeodjave/CurenjeMemorijeZbogNeodjave.cs
+++ CurenjeMemorijeZbogNeodjave/CurenjeMemorijeZbogNeodjave.cs
@@ -46,12 +46,13 @@ namespace CurenjeMemorijeZbogNeodjave
             {
                 SlušateljDogađaja sd = new SlušateljDogađaja();
 
-                // TODO:070 Pokrenuti kod i provjeriti ispis na konzolu.
+                // 070 Pokrenuti kod i provjeriti ispis na konzolu.
 
-                // TODO:071 Dodati naredbu kojom se objekt sd predbilježi na slušanje događaja, pokrenuti kod i provjeriti ispis.
+                // 071 Dodati naredbu kojom se objekt sd predbilježi na slušanje događaja, pokrenuti kod i provjeriti ispis.
+                gd.Događaj += sd.DogađajEventHandler;
 
-                // TODO:072 Dodati naredbu kojom se objekt sd odjavljuje od slušanja događaja, pokrenuti kod i provjeriti ispis.
-
+                // 072 Dodati naredbu kojom se objekt sd odjavljuje od slušanja događaja, pokrenuti kod i provjeriti ispis.
+                gd.Događaj -= sd.DogađajEventHandler;
             }
 
             Console.WriteLine($"Zauzeta memorija na heapu: {GC.GetTotalMemory(true) / 1024.0} kB");
