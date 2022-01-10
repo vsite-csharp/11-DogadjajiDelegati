def3498056af01fb4f9d126d2b275bf31f39a5cf
Ida Midžić <idmidzic@vsite.hr>
2022-01-10 22:04:50 +0100

    Riješeno DefiniranjeDogađaja

--- DefiniranjeDogadjaja/AlarmEventArgs.cs
+++ DefiniranjeDogadjaja/AlarmEventArgs.cs
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 namespace Vsite.CSharp.DogađajiDelegati
 {
     // TODO:064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
+    
     // TODO:065 U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
 
     public class AlarmEventArgs : EventArgs // po konvenciji AlarmEventArgs je izvedena klasa iz bazne EventArgs
     {
         public AlarmEventArgs(string mjesto, int razina, string opis)
         {
-            mjesto = Mjesto;
-            razina = Razina;
-            opis = Opis;
+            Mjesto = mjesto;
+            Razina = razina;
+            Opis = opis;
         }
         public readonly DateTime Vrijeme = DateTime.Now;
         public readonly String Mjesto;
--- DefiniranjeDogadjaja/GeneratorAlarma.cs
+++ DefiniranjeDogadjaja/GeneratorAlarma.cs
@@ -7,8 +7,8 @@ namespace Vsite.CSharp.DogađajiDelegati
 {
     // :060 U klasi GeneratorAlarma definirati događaj Alarm s delegatom tipa EventHandler te metodu protected virtual void OnAlarm(EventArgs e) koja treba okidati događaj Alarm.
 
-    // TODO:066 U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
-    // TODO:067 Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
+    // :066 U klasu GeneratorAlarma dodati definiciju delegata AlarmEventHandler koji kao drugi argument prima objekt tipa AlarmEventArgs.
+    // :067 Modificirati klasu GeneratorAlarma tako da je događaj Alarm tipa AlarmEventHandler.
     class GeneratorAlarma
     {
         //public event EventHandler Alarm;
