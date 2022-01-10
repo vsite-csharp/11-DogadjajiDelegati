8ccfa1d1059bc834d35b9dcceea51ef764273aa2
Ida Midžić <idmidzic@vsite.hr>
2022-01-10 22:05:39 +0100

    maknuto todo za DelegiranjeDogađaja

--- DefiniranjeDogadjaja/AlarmEventArgs.cs
+++ DefiniranjeDogadjaja/AlarmEventArgs.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
+    // :064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
     
-    // TODO:065 U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
+    // :065 U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
 
     public class AlarmEventArgs : EventArgs // po konvenciji AlarmEventArgs je izvedena klasa iz bazne EventArgs
     {
