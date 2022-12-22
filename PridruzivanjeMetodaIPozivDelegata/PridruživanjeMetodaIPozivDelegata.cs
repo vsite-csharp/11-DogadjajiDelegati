bf4b4c8993189d02ef888facf069bb66513b4fa0
Fran Štambuk <fran.stambuk@gmail.com>
2022-12-22 21:15:16 +0100

    Resolve delegate initialization

--- PridruzivanjeMetodaIPozivDelegata/PridruživanjeMetodaIPozivDelegata.cs
+++ PridruzivanjeMetodaIPozivDelegata/PridruživanjeMetodaIPozivDelegata.cs
@@ -33,33 +33,44 @@ namespace Vsite.CSharp.DogađajiDelegati
 
         public static void PozivStatičkeMetodePrekoDelegata()
         {
-            // TODO:010 Instancirati delegata tipa MojDelegat metodom StatičkaMetoda te pozvati delegata.
-
-            // TODO:011 Pokrenuti program i pogledati ispis.
+            // Instancirati delegata tipa MojDelegat metodom StatičkaMetoda te pozvati delegata.
+            MojDelegat md = StatičkaMetoda;
+            md();
+            // Pokrenuti program i pogledati ispis.
         }
 
         public static void PozivMetodeInstancePrekoDelegata()
         {
-            // TODO:012 Instancirati delegata tipa MojDelegat metodom MetodaInstance te pozvati delegata. 
-
-            // TODO:013 Pokrenuti program i provjeriti ispis.
+            // Instancirati delegata tipa MojDelegat metodom MetodaInstance te pozvati delegata. 
+            var inst = new PridruživanjeMetodaIPozivDelegata();
+            MojDelegat md = inst.MetodaInstance;
+            md();
+            // Pokrenuti program i provjeriti ispis.
         }
 
         public static void PozivStatičkeIDvijeMetodeInstancePrekoDelegata()
         {
-            // TODO:014 Instancirati delegata tipa MojDelegat metodom MetodaInstance. 
+            // Instancirati delegata tipa MojDelegat metodom MetodaInstance. 
+            var inst = new PridruživanjeMetodaIPozivDelegata();
+            MojDelegat md = inst.MetodaInstance;
+            //md();
+            md += StatičkaMetoda;
+            md += inst.DrugaMetodaInstance;
+            //md += DrugaStatičkaMetoda
+            md();
+            // Delegatu dodati metodu StatičkaMetoda i metodu DrugaMetodaInstance te pozvati delegata.
 
-            // TODO:015 Delegatu dodati metodu StatičkaMetoda i metodu DrugaMetodaInstance te pozvati delegata.
+            // Pokrenuti program i provjeriti ispis.
 
-            // TODO:016 Pokrenuti program i provjeriti ispis.
+            // Delegatu prije poziva pridružiti metodu DrugaStatičkaMetoda. Pogledati što se događa.
 
-            // TODO:017 Delegatu prije poziva pridružiti metodu DrugaStatičkaMetoda. Pogledati što se događa.
-
-            // TODO:018 Pokrenuti i provjeriti testove (3 testa u grupi "DelegatskeMetode" moraju proći).
+            // Pokrenuti i provjeriti testove (3 testa u grupi "DelegatskeMetode" moraju proći).
         }
 
         static void Main(string[] args)
         {
+            Console.OutputEncoding=Encoding.UTF8;
+
             PozivStatičkeMetodePrekoDelegata();
 
             Console.WriteLine();
