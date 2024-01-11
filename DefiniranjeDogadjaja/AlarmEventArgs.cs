951a203a15ecf77ad7223015dcadcc60da6cca56
Hrvoje Janđel <hrjandje@vsite.hr>
2024-01-11 21:32:17 +0100

    implement own events

--- DefiniranjeDogadjaja/AlarmEventArgs.cs
+++ DefiniranjeDogadjaja/AlarmEventArgs.cs
@@ -1,9 +1,20 @@
 ﻿namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
-    // TODO:065 U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
+    // :064 Klasu AlarmEventArgs definirati da je izvedena iz klase EventArgs. AlarmEventArgs treba omogućiti da se podaci o alarmu prenesu slušatelju.
+    // :065 U tu klasu dodati javna readonly polja Vrijeme (DateTime), Mjesto (string), Razina (int) i Opis (string) koja treba inicijalizirati u konstruktoru klase.
 
-    internal class AlarmEventArgs
+    internal class AlarmEventArgs:EventArgs
     {
+        public AlarmEventArgs (string mjesto, int razina, string opis)
+        {
+            Vrijeme=DateTime.Now;
+            Mjesto=mjesto;
+            Razina=razina;
+            Opis=opis;
+        }
+        public readonly DateTime Vrijeme;
+        public readonly string Mjesto;
+        public readonly int Razina;
+        public readonly string Opis;
     }
 }
--- DefiniranjeDogadjaja/SlušateljAlarma.cs
+++ DefiniranjeDogadjaja/SlušateljAlarma.cs
@@ -1,12 +1,15 @@
 ﻿namespace Vsite.CSharp.DogađajiDelegati
 {
-    // TODO:062 U klasu SlušateljAlarma dodati metodu AlarmNotifikacija koja će se moći predbilježiti na događaj Alarm u klasi GeneratorAlarma. 
+    // :062 U klasu SlušateljAlarma dodati metodu AlarmNotifikacija koja će se moći predbilježiti na događaj Alarm u klasi GeneratorAlarma. 
     // Unutar te metode na konzolu ispisati poruku da je dobivena poruka o alarmu
     internal class SlušateljAlarma
     {
-
-        // TODO:068 Modificirati metodu AlarmNotifikacija tako da ispiše sve podatke o alarmu (vrijeme, mjesto, razinu i opis). Pokrenuti program i provjeriti ispis.
+        public void AlarmNotifikacija (object? sender, AlarmEventArgs e)
+        {
+            Console.WriteLine($"Vrijeme: {e.Vrijeme},Mjesto: {e.Mjesto}, Razina: {e.Razina}, Opis: {e.Opis}");
+        }
+        // :068 Modificirati metodu AlarmNotifikacija tako da ispiše sve podatke o alarmu (vrijeme, mjesto, razinu i opis). Pokrenuti program i provjeriti ispis.
     }
 
-    // TODO:069 Pokrenuti testove u grupi DefiniranjeDogađaja (5 testova mora proći).
+    // :069 Pokrenuti testove u grupi DefiniranjeDogađaja (5 testova mora proći).
 }
