0fe4e848ecf7f2daa424bef9973c8fa1b5b477cc
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-01-11 20:29:05 +0100

    - do UlančavanjeDelegata

--- UlancavanjeDelegata/UlančavanjeDelegata.cs
+++ UlancavanjeDelegata/UlančavanjeDelegata.cs
@@ -22,15 +22,17 @@
 
             ispisPoruke(poruke[0]);
 
-            // TODO:030 Pokrenuti program i pogledati sadržaj datoteke "poruke.txt" (u potkazalu bin\Debug).
+            // :030 Pokrenuti program i pogledati sadržaj datoteke "poruke.txt" (u potkazalu bin\Debug).
 
-            // TODO:031 Dodajte delegatu metodu IspišiNaKonzolu.
+            ispisPoruke += IspišiNaKonzolu;
+            // :031 Dodajte delegatu metodu IspišiNaKonzolu.
 
             ispisPoruke(poruke[1]);
 
-            // TODO:032 Pokrenite program i usporedite prikaz na konzoli sa sadržajem datoteke "poruke.txt".
+            // :032 Pokrenite program i usporedite prikaz na konzoli sa sadržajem datoteke "poruke.txt".
 
-            // TODO:033 Iz delegata uklonite IspišiUFajl, pokrenite program i provjerite sadržaj datoteke "poruke.txt".
+            ispisPoruke -= IspišiUFajl;
+            // :033 Iz delegata uklonite IspišiUFajl, pokrenite program i provjerite sadržaj datoteke "poruke.txt".
 
             ispisPoruke(poruke[2]);
         }
@@ -46,7 +48,7 @@
 
         public static TextWriter writer;
 
-        // TODO:034 Pokrenite i provjerite testove (dva testa u grupi "UlančavanjaDelegata" moraju proći)
+        // :034 Pokrenite i provjerite testove (dva testa u grupi "UlančavanjaDelegata" moraju proći)
         static void Main()
         {
             IspisPoruka("Dobar dan", "Bok", "Guten Tag");
