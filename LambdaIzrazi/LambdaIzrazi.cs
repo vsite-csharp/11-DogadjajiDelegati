a9d0b35c7c0d09bc75123f9b9de81ea4b15b0bd7
Ivan Davidović <hedead421@gmail.com>
2024-01-11 21:03:20 +0100

    Implement lambda expretions

--- LambdaIzrazi/LambdaIzrazi.cs
+++ LambdaIzrazi/LambdaIzrazi.cs
@@ -12,27 +12,33 @@
             return rezultat;
         }
 
-        // TODO:050 U donjoj metodi pozvati metodu PrimijeniOperaciju tako da vrati niz korijenovanih članova zadanog niza.
+        // :050 U donjoj metodi pozvati metodu PrimijeniOperaciju tako da vrati niz korijenovanih članova zadanog niza.
         public static IEnumerable<double> KorijenujČlanoveNiza(IEnumerable<double> niz)
         {
-            return niz;
+            //List<double> rezultat = new List<double>();
+            //foreach (double broj in niz)
+            //{
+            //    rezultat.Add(Math.Sqrt(broj));
+            //}
+            //return rezultat;
+            return niz.Select(x => Math.Sqrt(x));
         }
 
-        // TODO:051 U donjoj metodi pozvati metodu PrimijeniOperaciju tako da vrati niz kvadrata članova zadanog niza.
+        // :051 U donjoj metodi pozvati metodu PrimijeniOperaciju tako da vrati niz kvadrata članova zadanog niza.
         public static IEnumerable<double> KvadrirajČlanoveNiza(IEnumerable<double> niz)
         {
-            return niz;
-        }
+            return niz.Select(x => x * x);
+		}
 
-        // TODO:052 U donjoj metodi pozvati metodu PrimijeniOperaciju tako da vrati niz riječi iz zadanog niza sa svim velikim slovima.
+        // :052 U donjoj metodi pozvati metodu PrimijeniOperaciju tako da vrati niz riječi iz zadanog niza sa svim velikim slovima.
         public static IEnumerable<string> Kapitaliziraj(IEnumerable<string> niz)
         {
-            return niz;
-        }
+            return niz.Select(x => x.ToUpper());
+		}
 
-        // TODO:053 Pozive metode PrimijeniOperaciju u gornjim metodama zamijenite pozivima metode proširenja IEnumerable<T>.Select.
+        // :053 Pozive metode PrimijeniOperaciju u gornjim metodama zamijenite pozivima metode proširenja IEnumerable<T>.Select.
 
-        // TODO:054 Pokrenuti i provjeriti testove (3 testa u grupi "LambdaIzrazi" moraju proći).
+        // :054 Pokrenuti i provjeriti testove (3 testa u grupi "LambdaIzrazi" moraju proći).
 
         static void Main()
         {
